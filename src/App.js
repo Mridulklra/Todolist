3e403867ba29080319eb36cf46e273a98f7fbe19
Mridul kalra <118498883+Mridulklra@users.noreply.github.com>
2023-09-05 00:00:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Todo from './components/todo'
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+
+    <Todo/>
+    </>
+    
+  );
+}
+
+export default App;
